fca8aed1146a0e519be1d3f4d0c4b0cdd71e70b1
shihaio <shihao.mymail@gmail.com>
2022-11-30 23:34:58 +0800

    add file

---
+++ draft/script.js
@@ -0,0 +1,51 @@
+// $(document).ready(printLabel);
+
+$(() => {
+  let timer = 60; // create hero
+  let interval;
+
+  function printLabel() {
+    console.log(timer);
+    $("#timer").text(timer); // insert text into html!
+  }
+
+  function addInterval() {
+    interval = setInterval(() => {
+      if (timer === 0) {
+        clearInterval(interval);
+        return;
+      }
+
+      timer--;
+      printLabel();
+    }, 1000);
+  }
+
+  printLabel(); // call functions
+  addInterval(); // call functions
+});
+
+// const dinosaurs = [
+//   "T-Rex",
+//   "Velociraptor",
+//   "Stegosaurus",
+//   "Triceraptors",
+//   "Branchiosaurus",
+//   "Pteranodon",
+//   "Apatosaurus",
+//   "Diplodocus",
+//   "Compsognathus",
+// ];
+
+// // console.log(dinosaurs);
+// // console.log(dinosaurs[5]);
+// // console.log(dinosaurs[1].length);
+
+// dinosaurs.forEach((apple) => {
+//   console.log(apple);
+// });
+
+// for (i = 0; i < dinosaurs.length; i++) {
+//   console.log(dinosaurs[i]);
+
+// }
--- script.js
+++ script.js
@@ -1,51 +1,25 @@
-// // $(document).ready(printLabel);
-
-// $(() => {
-//   let timer = 60; // create hero
-//   let interval;
-
-//   function printLabel() {
-//     console.log(timer);
-//     $("#timer").text(timer); // insert text into html!
-//   }
-
-//   function addInterval() {
-//     interval = setInterval(() => {
-//       if (timer === 0) {
-//         clearInterval(interval);
-//         return;
-//       }
-
-//       timer--;
-//       printLabel();
-//     }, 1000);
-//   }
-
-//   printLabel(); // call functions
-//   addInterval(); // call functions
-// });
-
-const dinosaurs = [
-  "T-Rex",
-  "Velociraptor",
-  "Stegosaurus",
-  "Triceraptors",
-  "Branchiosaurus",
-  "Pteranodon",
-  "Apatosaurus",
-  "Diplodocus",
-  "Compsognathus",
+// $(document).ready(printLabel);
+const codeList = [
+  `dinosaurs.forEach((apple) => {
+  console.log(apple); 
+});`,
+  `$("p").remove(".test, .demo");`,
+  `$(this).removeClass("incorrect")`,
+  `while (alive) {
+    east();
+    sleep();
+    code();
+    repeat();
+  }`,
 ];
-
-// console.log(dinosaurs);
-// console.log(dinosaurs[5]);
-// console.log(dinosaurs[1].length);
-
-// dinosaurs.forEach((apple) => {
-//   console.log(apple);
-// });
-
-for (i = 0; i < dinosaurs.length; i++) {
-  console.log(dinosaurs[i]);
-  
+//Select Element:
+const $displayCodeBox = $(".display-code");
+// display code
+function displayNewCode() {
+  // get random index from array
+  // display random element
+  $displayCodeBox.text(codeList[0]);
 }
+$(() => {
+  displayNewCode();
+});
