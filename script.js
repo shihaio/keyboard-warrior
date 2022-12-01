8ee8536b40299f277b169f7de2cb8a6246780612
shihaio <shihao.mymail@gmail.com>
2022-12-01 21:37:14 +0800

    feat: added eventListener to textarea element with class input-code

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ const codeList = [
 ];
 //Select Element:
 const $displayCodeBox = $(".display-code");
+const $inputCode = $(".input-code");
+
+$inputCode.on("input", () => console.log($inputCode.val()));
+// $inputCode.val().on("input");
 
 // display new code
 function displayNewCode() {
@@ -21,6 +25,23 @@ function displayNewCode() {
   $displayCodeBox.text(codeList[randomIndex]);
 }
 
+// transfer displayNewCode a inputNewCode
+function transferCode() {
+  let typedCode = $inputCode.val();
+  return typedCode;
+}
+// create compare value function
+// displayCodeBox VS
+
+// const typedCode = $(".input-code").val();
+// console.log(typedCode);
+// create a function that compare between displayNewCode and inputNewCode
+
+// Check acurracy
+// Move to next round
+
 $(() => {
   displayNewCode();
+  const codeInput = transferCode();
+  console.log(codeInput);
 });
